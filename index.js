668185c96d548679994fef04e6b819a085854367
Bogodar Shmyglyk <bogodar@gmail.com>
2020-11-29 02:36:51 +0200

    create/upload file 404 server error fixed

--- index.js
+++ index.js
@@ -7,23 +7,24 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-const CONNECTION_URL =
-  'mongodb+srv://bogodar:HjeYU7qq2pfestd@cluster0.mchbn.mongodb.net/<dbname>?retryWrites=true&w=majority';
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = `mongodb+srv://bogodar:HjeYU7qq2pfestd@cluster0.mchbn.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
   )
-  .catch((error) => console.log(error.message));
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
 
