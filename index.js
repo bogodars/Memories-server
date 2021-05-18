39f93fca9388f7ba61879f6c9e091a6bad315e6d
Bogodar Shmyglyk <bogodar@gmail.com>
2021-05-18 17:38:54 +0300

    make it better

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = `mongodb+srv://bogodar:HjeYU7qq2pfestd@cluster0.mchbn.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+app.get('/', (req, res) => {
+  res.send('You are fine now');
+});
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
@@ -27,5 +33,3 @@ mongoose
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
-
-//HjeYU7qq2pfestd pass mongo
